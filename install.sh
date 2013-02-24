2cf270927d755c927954f08547de9e6591d2230e
randomfish <mmarr013@gmail.com>
2013-02-24 08:52:31 -0500

    changes

--- install.sh
+++ install.sh
@@ -42,5 +42,5 @@ git submodule update --init --recursive
 git submodule foreach --recursive git pull origin master
 
 setup command-t
-cd _vim/bundle/command-t
+cd ~/.vim/bundle/command-t
 rake make
